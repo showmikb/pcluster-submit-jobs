29640adc7046729b0a7734dbe4727c7c09af6fc4
Showmik Bose <officialshowmik@gmail.com>
2021-06-23 22:56:57 +0530

    Update pclustermanage.sh

--- pclustermanage.sh
+++ pclustermanage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pcluster ssh dmscluster -i divine-showmik.pem 'bash -s' < main.sh
+pcluster -r us-east-1 ssh dmscluster -i divine-showmik.pem 'bash -s' < main.sh
 
 
 sudo yum install git -y
