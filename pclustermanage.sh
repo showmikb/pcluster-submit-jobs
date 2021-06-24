27363ae883acb1bf01477e4db772b1089e03742b
Showmik Bose <officialshowmik@gmail.com>
2021-06-24 10:29:58 +0530

    Update pclustermanage.sh

--- pclustermanage.sh
+++ pclustermanage.sh
@@ -5,6 +5,4 @@ pcluster  ssh dmscluster -r us-east-1 -i divine-showmik.pem 'bash -s' < main.sh
 
 
 #sudo yum install git -y
-#git clone https://github.com/showmikb/pcluster-submit-jobs.git
-cd pcluster-submit-jobs
-sh main.sh
+
