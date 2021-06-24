99e47db790b8cb34b6d61f67f95251ac9c29cad8
Showmik Bose <officialshowmik@gmail.com>
2021-06-24 10:27:03 +0530

    Update pclustermanage.sh

--- pclustermanage.sh
+++ pclustermanage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export  PATH='/sbin:/bin:/usr/sbin:/usr/bin:~/.local/bin/'
+chmod 600 divine-showmik.pem
 pcluster  ssh dmscluster -r us-east-1 -i divine-showmik.pem 
 #'bash -s' < main.sh
 
