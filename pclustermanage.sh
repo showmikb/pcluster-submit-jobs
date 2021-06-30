cca33fc976ddc7814096f542b737f3b4e5ee99e4
Showmik Bose <officialshowmik@gmail.com>
2021-06-30 23:24:27 +0530

    Update pclustermanage.sh

--- pclustermanage.sh
+++ pclustermanage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export  PATH='/sbin:/bin:/usr/sbin:/usr/bin:~/.local/bin/'
 echo "DOWNLOADFROM S3"
-aws s3 cp s3://s3pemstore/divine-showmik.pem .
+aws s3 cp s3://divinopem/divine-showmik.pem .
 chmod 600 divine-showmik.pem
 pcluster  ssh dmscluster -r us-east-1 -i divine-showmik.pem 'bash -s' < main.sh
 
