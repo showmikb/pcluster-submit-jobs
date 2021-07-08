58f984bbec6c195d6c14f27b77dca6f213ca5f89
Showmik Bose <officialshowmik@gmail.com>
2021-07-08 20:22:59 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #Append your authentication token if it is a private repo : git clone https://token@github.com/showmikb/pcluster-submit-jobs.git
-DIR="pcluster-submit-job"
+DIR="pcluster-submit-jobs"
 if [ -d "$DIR" ]; then
   # Take action if $DIR exists. #
   echo "${DIR} Present"
   cd $DIR
-  git pull https://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@github.com/showmikb/pcluster-submit-jobs.git
+  git pull https://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@github.com/showmikb/$DIR.git
 
 else 
   echo "Directory $DIR Not Present"
-  git clone https://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@github.com/showmikb/pcluster-submit-jobs.git
+  git clone https://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@github.com/showmikb/$DIR.git
   cd $DIR
 fi
 
