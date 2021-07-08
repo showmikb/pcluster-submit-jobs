6bc0e947ed6cda73e9a7275f512bf99937db4911
Showmik Bose <officialshowmik@gmail.com>
2021-07-08 19:52:14 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Append your authentication token if it is a private repo : git clone https://token@github.com/showmikb/pcluster-submit-jobs.git
-git clone https://github.com/showmikb/pcluster-submit-jobs.git
+git clone https://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@github.com/showmikb/pcluster-submit-jobs.git
 cd pcluster-submit-jobs
 echo "Submitting Jobs"
 qsub sampleshell.sh
